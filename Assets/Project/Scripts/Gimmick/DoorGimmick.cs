57b155fc34fd63e108d7a079175b556e01346b7a
Fumihito Ushimaru <76957710+AirLAlpha@users.noreply.github.com>
2023-05-14 01:25:56 +0900

    20230514-2

--- Assets/Project/Scripts/Gimmick/DoorGimmick.cs
+++ Assets/Project/Scripts/Gimmick/DoorGimmick.cs
@@ -68,11 +68,8 @@ public class DoorGimmick : ReceiveGimmick
 	private void Update()
 	{
 		//	座標を保持しておく
-<<<<<<< HEAD
 		savePos = doorRoot.localPosition;
-=======
-		//savePos = doorRoot.localPosition;
->>>>>>> 61bb0e1f8d4cd31ec5078bfe6af5c9e89e009668
+
 
 		//	ドアの開閉状態によって進行度を更新する
 		if (isOpen)
@@ -85,7 +82,6 @@ public class DoorGimmick : ReceiveGimmick
 		}
 		openProgress = Mathf.Clamp01(openProgress);
 		//	進行度から座標を算出する（進行度にはイージングを適応）
-<<<<<<< HEAD
 		doorRoot.localPosition = Vector3.Lerp(Vector3.zero, openOffset, EasingFunctions.EaseInOutSine(openProgress));
 
 		//var b = Vector3.Lerp(Vector3.zero, openOffset, EasingFunctions.EaseInOutSine(openProgress));
@@ -93,13 +89,6 @@ public class DoorGimmick : ReceiveGimmick
 		//doorRootRb.MovePosition(a);
 
 		CheckOnRodeObject();
-=======
-		//doorRoot.localPosition = Vector3.Lerp(Vector3.zero, openOffset, EasingFunctions.EaseInOutSine(openProgress));
-
-		var b = Vector3.Lerp(Vector3.zero, openOffset, EasingFunctions.EaseInOutSine(openProgress));
-		var a = transform.TransformPoint(b);
-		doorRootRb.MovePosition(a);
->>>>>>> 61bb0e1f8d4cd31ec5078bfe6af5c9e89e009668
 	}
 
 	/*--------------------------------------------------------------------------------
